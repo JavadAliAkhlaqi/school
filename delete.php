55185f8b924bafb493114bd554b4686aa3e012d3
JavadAliAkhlaqi <javadaliakhlaqi@gmail.com>
2015-04-18 08:48:07 -0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../db_connection/connection.php");
+include_once("connection.php");
 $id=$_GET["id"];
 $query="delete from student where idstudent=$id";
 if(mysql_query($query)){
