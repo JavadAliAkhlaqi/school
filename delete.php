9dcc08acffb96cc42ec59ed7a7266bbcb50a8a1f
JavadAliAkhlaqi <javadaliakhlaqi@gmail.com>
2015-04-18 08:43:36 -0700

    Create delete.php
    this is the delete page of my template of the database

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+include_once("../db_connection/connection.php");
+$id=$_GET["id"];
+$query="delete from student where idstudent=$id";
+if(mysql_query($query)){
+header("Location:http://localhost/w3_project/first_layer/index.php?title=student");
+}else{
+echo $query;
+
+}
+
+?>
